703cba855c597796bf8cb8861b0bdab0b592ae59
“garrettruble1” <garrettruble1@gmail.com>
2020-05-29 18:13:37 -0400

    Handling Events Activity

--- src/ToDo.js
+++ src/ToDo.js
@@ -1,11 +1,12 @@
 import React from 'react';
 
 function ToDo (props) {
+
     return (
         <div className="listContainer">
           <ul className="navList">
             <h2 className="listHeader">Groceries</h2>
-            <li className="listItem listItemFlex"><button className="listItemText"><i className="fas fa-pen fa-sm icon"></i>Add Item...</button></li>
+            <li className="listItem listItemFlex"><input type="text" className="addToList" placeholder="Add new item..." value={props.newItem} onChange={props.inputHandler} onKeyDown={props.enterHandler}></input><button className="listItemMore" onClick={props.clickHandler}><i className="fas fa-plus-circle icon"></i>Add</button></li>
             {props.list.map((item)=>(
               <li className="listItem listItemFlex">
                 <button className="listItemText">
